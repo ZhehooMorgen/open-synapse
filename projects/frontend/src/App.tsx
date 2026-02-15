6a35ea981e7a583ae3d0ab53d530e273b3279509
Zhehoo Morgen <1318103969@qq.com>
2026-02-16 00:08:45 +0800

    formatter

--- projects/frontend/src/App.tsx
+++ projects/frontend/src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from "react"
-import { Button } from "./shadcn/ui/button"
+import { useState } from "react";
+import { Button } from "./shadcn/ui/button";
 
 export default function App() {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="font-medium">Hello World</div>
       <div className="font-medium">Count = {counter}</div>
-      <Button
-        onClick={() => setCounter(counter + 1)}
-      >
-        Increment
-      </Button>
+      <Button onClick={() => setCounter(counter + 1)}>Increment</Button>
     </div>
-  )
+  );
 }
