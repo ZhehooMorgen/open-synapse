7af7b6bc7be631e121149f2ffcd4ab1f948e8105
Zhehoo Morgen <1318103969@qq.com>
2026-02-15 23:50:53 +0800

    Frontend phase 2 config shadcn

--- projects/frontend/src/App.tsx
+++ projects/frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Button } from "./shadcn/ui/button"
 
 export default function App() {
   const [counter, setCounter] = useState(0)
@@ -6,12 +7,11 @@ export default function App() {
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="font-medium">Hello World</div>
       <div className="font-medium">Count = {counter}</div>
-      <button
-        className="px-4 py-2 ml-4 text-white bg-blue-500 rounded hover:bg-blue-600"
+      <Button
         onClick={() => setCounter(counter + 1)}
       >
         Increment
-      </button>
+      </Button>
     </div>
   )
 }
