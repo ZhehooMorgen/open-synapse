ab3b872679eb86ebfaccb7eb6d68c584f551aa1b
Zhehoo Morgen <1318103969@qq.com>
2026-02-16 00:30:41 +0800

    Add redis

--- projects/backend/ConfigReader.ts
+++ projects/backend/ConfigReader.ts
@@ -10,18 +10,12 @@ export interface IConfigReader {
 
 class ConfigReader implements IConfigReader {
   getConfig() {
-    const port = Number(process.env.PORT);
-    const databaseUrl = `postgresql://${process.env.pgsqlUser}:${process.env.pgsqlPassword}@localhost:${process.env.pgsqlPort}/${process.env.pgsqlDatabase}`;
-    const restatePort = Number(process.env.RESTATE_PORT);
-    const restateAdminPort = Number(process.env.RESTATE_ADMIN_PORT);
-    const restateWorkerPort = Number(process.env.RESTATE_WORKER_PORT);
-
     return {
-      port,
-      databaseUrl,
-      restatePort,
-      restateAdminPort,
-      restateWorkerPort,
+      port: Number(process.env.PORT),
+      databaseUrl: `postgresql://${process.env.pgsqlUser}:${process.env.pgsqlPassword}@localhost:${process.env.pgsqlPort}/${process.env.pgsqlDatabase}`,
+      restatePort: Number(process.env.RESTATE_PORT),
+      restateAdminPort: Number(process.env.RESTATE_ADMIN_PORT),
+      restateWorkerPort: Number(process.env.RESTATE_WORKER_PORT),
     };
   }
 }
